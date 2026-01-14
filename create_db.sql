53a70a36315bd2db87dc56d0cc4e2bfbb9461787
d9394 <15172911+d9394@users.noreply.github.com>
2026-01-14 16:32:12 +0800

    Refactor database schema to use InnoDB engine

--- create_db.sql
+++ create_db.sql
@@ -1,15 +1,3 @@
-CREATE TABLE `lastpacket` (
-  `call` varchar(16) NOT NULL,
-  `tm` timestamp NOT NULL DEFAULT current_timestamp(),
-  `datatype` char(1) NOT NULL,
-  `lat` char(8) NOT NULL,
-  `lon` char(9) NOT NULL,
-  `table` char(1) NOT NULL,
-  `symbol` char(1) NOT NULL,
-  `msg` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `speed` int(6) DEFAULT 0,
-  PRIMARY KEY (`call`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `aprspacket` (
   `tm` timestamp NOT NULL DEFAULT current_timestamp(),
@@ -19,23 +7,38 @@ CREATE TABLE `aprspacket` (
   `lon` char(9) NOT NULL,
   `table` char(1) NOT NULL,
   `symbol` char(1) NOT NULL,
-  `msg` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `raw` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `msg` varchar(200) NOT NULL,
+  `raw` varchar(250) NOT NULL,
   KEY `tm` (`tm`),
   KEY `tm_call` (`tm`,`call`),
-  KEY `call_tm` (`call`,`tm`),
-  KEY `tm_lat` (`tm`,`lat`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  KEY `call_tm` (`call`,`tm`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `aprspackethourcount` (
   `tm` timestamp NOT NULL DEFAULT current_timestamp(),
   `call` varchar(16) NOT NULL,
   `pkts` int(10) DEFAULT NULL,
   PRIMARY KEY (`tm`,`call`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `lastpacket` (
+  `call` varchar(16) NOT NULL,
+  `tm` timestamp NOT NULL DEFAULT current_timestamp(),
+  `datatype` char(1) NOT NULL,
+  `lat` char(8) NOT NULL,
+  `lon` char(9) NOT NULL,
+  `table` char(1) NOT NULL,
+  `symbol` char(1) NOT NULL,
+  `msg` varchar(200) NOT NULL,
+  `speed` smallint(5) unsigned DEFAULT NULL COMMENT 'Speed in km/h',
+  PRIMARY KEY (`call`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `packetstats` (
   `day` date NOT NULL DEFAULT '0000-00-00',
   `packets` int(10) DEFAULT NULL,
   PRIMARY KEY (`day`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
